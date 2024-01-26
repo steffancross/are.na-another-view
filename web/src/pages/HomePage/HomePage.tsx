32d3adc51e216ae6132c073cece7666ba6f66b43
Steffan Cross <steffancross@berkeley.edu>
2024-01-25 22:53:02 -0800

    changed how the functions work a bit, user now just pastes channel link instead of the slug

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,9 @@ import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
-  const fetchData = async (data) => {
+  const fetchData = async (slug) => {
     try {
-      const response = await fetch(
-        `http://api.are.na/v2/channels/${data.channelLink}`
-      )
+      const response = await fetch(`http://api.are.na/v2/channels/${slug}`)
       const result = await response.json()
       console.log(result)
     } catch (error) {
@@ -15,8 +13,12 @@ const HomePage = () => {
     }
   }
 
-  const onSubmit = (link) => {
-    fetchData(link)
+  const onSubmit = ({ channelLink }) => {
+    const splitLink = channelLink.split('/')
+    const slug = splitLink[splitLink.length - 1]
+    console.log(slug)
+
+    fetchData(slug)
   }
 
   return (
@@ -27,7 +29,7 @@ const HomePage = () => {
       <p>testing testing</p>
       <Link to={routes.about()}>ABOUT</Link>
       <Form onSubmit={onSubmit}>
-        <label>Channel Link</label>
+        <label>Channel Link/Slug</label>
         <InputField name="channelLink"></InputField>
         <Submit>GO</Submit>
       </Form>
