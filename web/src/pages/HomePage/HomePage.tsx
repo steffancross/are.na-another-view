344ab5035c2dcb78b3840c6d5cecb4b134731da6
Steffan Cross <steffancross@berkeley.edu>
2024-01-25 21:45:32 -0800

    input form working

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,24 @@
+import { Form, InputField, Submit } from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
+  const onSubmit = () => {
+    console.log('hello')
+  }
+
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
-      <h1>Are.na Viewer</h1>
+      <h1>Another Viewer</h1>
       <p>testing testing</p>
       <Link to={routes.about()}>ABOUT</Link>
+      <Form onSubmit={onSubmit}>
+        <label>Channel Link</label>
+        <InputField name="input"></InputField>
+        <Submit>GO</Submit>
+      </Form>
     </>
   )
 }
