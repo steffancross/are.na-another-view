ed218a86766aef5c1cb1fb05371c48e52c0227a8
Steffan Cross <steffancross@berkeley.edu>
2024-01-28 18:48:33 -0800

    can now zoom and pan on canvas

--- web/src/components/Canvas/Canvas.tsx
+++ web/src/components/Canvas/Canvas.tsx
@@ -23,9 +23,48 @@ const Canvas = () => {
         canvas.add(oImg)
       })
     })
+
+    // Zooming with mousewheel
+    canvas.on('mouse:wheel', function (opt) {
+      const delta = opt.e.deltaY
+      let zoom = canvas.getZoom()
+      zoom *= 0.999 ** delta
+      if (zoom > 20) zoom = 20
+      if (zoom < 0.01) zoom = 0.01
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
+      opt.e.preventDefault()
+      opt.e.stopPropagation()
+    })
+
+    // Panning holding alt
+    canvas.on('mouse:down', function (opt) {
+      const evt = opt.e
+      if (evt.altKey === true) {
+        this.isDragging = true
+        this.selection = false
+        this.lastPosX = evt.clientX
+        this.lastPosY = evt.clientY
+      }
+    })
+    canvas.on('mouse:move', function (opt) {
+      if (this.isDragging) {
+        const e = opt.e
+        const vpt = this.viewportTransform
+        vpt[4] += e.clientX - this.lastPosX
+        vpt[5] += e.clientY - this.lastPosY
+        this.requestRenderAll()
+        this.lastPosX = e.clientX
+        this.lastPosY = e.clientY
+      }
+    })
+    canvas.on('mouse:up', function (opt) {
+      this.setViewportTransform(this.viewportTransform)
+      this.isDragging = false
+      this.selection = true
+    })
   })
 
-  return <canvas width={600} height={600} ref={canvasRef} />
+  return <canvas ref={canvasRef} />
 }
 
 export default Canvas
