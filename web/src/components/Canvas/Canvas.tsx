7734bfc51a64a8b931e08591c01cc3e4ae74704b
Steffan Cross <steffancross@berkeley.edu>
2024-01-28 17:50:40 -0800

    images showing up on canvas correctly and are draggable and resizeable

--- web/src/components/Canvas/Canvas.tsx
+++ web/src/components/Canvas/Canvas.tsx
@@ -1,15 +1,31 @@
+import { useRef, useEffect } from 'react'
+
+import { fabric } from 'fabric'
+
 const Canvas = () => {
   const data = [
     'https://upload.wikimedia.org/wikipedia/commons/2/27/Red_square.svg',
     'https://upload.wikimedia.org/wikipedia/commons/2/27/Red_square.svg',
   ]
-  return (
-    <div>
-      {data.map((item, index) => (
-        <img src={item} alt="" key={index}></img>
-      ))}
-    </div>
-  )
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      height: height,
+      width: width,
+    })
+
+    data.map((item) => {
+      fabric.Image.fromURL(item, function (oImg) {
+        canvas.add(oImg)
+      })
+    })
+  })
+
+  return <canvas width={600} height={600} ref={canvasRef} />
 }
 
 export default Canvas
